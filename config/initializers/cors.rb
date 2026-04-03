e50e3741144ed80f7954914743b4f25f0e27f530
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-03 15:33:20 +0530

    fixed cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mini-app-frontend-three.vercel.app/'
+    origins 'https://mini-app-frontend-three.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
