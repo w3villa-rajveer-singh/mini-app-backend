2ee13ed42e2b758d562d8de43166b84df86210d5
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-03 14:58:56 +0530

    changed the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2026_03_31_104210) do
-  create_table "jwt_denylists", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "jwt_denylists", force: :cascade do |t|
     t.string "jti"
     t.datetime "exp"
     t.datetime "created_at", null: false
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_31_104210) do
     t.index ["jti"], name: "index_jwt_denylists_on_jti"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
