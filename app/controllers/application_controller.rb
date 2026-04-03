b2120bba030e058eb31d617588556a7e49d97b17
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-03 18:34:54 +0530

    Fix JWT current_user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,29 @@ class ApplicationController < ActionController::API
 
   private
 
-  def store_location_for(resource, location)
-    # do nothing (disable session storage)
+  # 🔥 Decode JWT and set current_user
+  def current_user
+    return @current_user if defined?(@current_user)
+
+    header = request.headers['Authorization']
+    return nil unless header
+
+    token = header.split(' ').last # removes "Bearer"
+
+    begin
+      decoded = JWT.decode(token, Rails.application.secret_key_base)[0]
+      @current_user = User.find(decoded['sub'])
+    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+      @current_user = nil
+    end
   end
 
+  # 🔥 Use this for auth
   def authenticate_user!
     render json: { error: 'Not Authorized' }, status: :unauthorized unless current_user
+  end
+
+  def store_location_for(resource, location)
+    # disable session storage
   end
 end
