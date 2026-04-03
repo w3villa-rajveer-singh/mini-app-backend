b60459fac714f83fed6eb3fd73db744a333bba92
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-03 19:41:54 +0530

    Fix JWT user_id extraction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,21 +6,24 @@ class ApplicationController < ActionController::API
   private
 
   # 🔥 Decode JWT and set current_user
-  def current_user
-    return @current_user if defined?(@current_user)
+ def current_user
+  return @current_user if defined?(@current_user)
 
-    header = request.headers['Authorization']
-    return nil unless header
+  header = request.headers['Authorization']
+  return nil unless header
 
-    token = header.split(' ').last # removes "Bearer"
+  token = header.split(' ').last
 
-    begin
-      decoded = JWT.decode(token, Rails.application.secret_key_base)[0]
-      @current_user = User.find(decoded['sub'])
-    rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-      @current_user = nil
-    end
+  begin
+    decoded = JWT.decode(token, Rails.application.secret_key_base)[0]
+
+    user_id = decoded['sub'] || decoded['user_id'] || decoded['id']
+
+    @current_user = User.find(user_id)
+  rescue JWT::DecodeError, ActiveRecord::RecordNotFound
+    @current_user = nil
   end
+end
 
   # 🔥 Use this for auth
   def authenticate_user!
