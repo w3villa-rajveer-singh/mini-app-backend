969831def228546951d549294ea7ffdb30382155
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-03 17:17:04 +0530

    Add SendGrid SMTP config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,22 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { 
   host: 'mini-app-backend-mkjh.onrender.com',
   protocol: 'https'
-}
+  }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.sendgrid.net",
+  port: 587,
+  domain: "mini-app-backend-mkjh.onrender.com",
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"],
+  authentication: :plain,
+  enable_starttls_auto: true
+  }
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
