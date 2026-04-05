c36fcbbb631606531301d26ca278221a74a8b0e9
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-05 15:49:37 +0530

    fixed jwt issue

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,29 +1,16 @@
-require 'jwt'
-
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
   private
 
-  def current_token
-    # Generate JWT token manually
-    payload = { 
-      sub: resource.id, 
-      user_id: resource.id,
-      exp: 1.day.from_now.to_i 
-    }
-    JWT.encode(payload, Rails.application.secret_key_base)
-  end
-
   def respond_with(resource, _opts = {})
     render json: {
       message: 'Logged in successfully',
-      user: resource,
-      token: current_token
+      user: resource
     }, status: :ok
   end
 
-  def respond_to_on_destroy(_resource_name)
+  def respond_to_on_destroy
     render json: {
       message: 'Logged out successfully'
     }, status: :ok
