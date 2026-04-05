c6d5aaadc9432e59ca6efdb66b6ee7a20e5e66b0
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-05 23:48:57 +0530

    fixed issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,10 @@
 OmniAuth.config.allowed_request_methods = [:get, :post]
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2,
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET'],
+    {
+      callback_url: ENV['GOOGLE_CALLBACK_URL']
+    }
+end
