695f77ee00bdb16594fd21c7d78844b0010b6812
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-06 15:59:24 +0530

    Revert "issue fixed"
    This reverts commit 9a6447e1c569b1c3f3b7069a2825942acd07f68a.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,14 +13,13 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      sign_in(@user)
+      sign_in(@user)  # ✅ Devise generates JWT
 
       token = request.env['warden-jwt_auth.token']
-      token ||= Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil).first
 
       redirect_to "#{frontend_url}/social-login?token=#{token}"
     else
-      redirect_to "#{frontend_url}/login"
+      redirect_to "/login"
     end
   end
 
