ce1d00b483146b34019e251ae8707dca611b34d0
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-07 11:04:11 +0530

    fixed at 11am

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,7 +12,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_auth
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
-    if @user.persisted?
+    if @user && @user.persisted?
     sign_in(@user, scope: :user)
 
     token = Warden::JWTAuth::UserEncoder.new.call(@user, :user, nil).first
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,27 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable,
-       :confirmable,
-       :jwt_authenticatable,
-       :omniauthable,
-       jwt_revocation_strategy: JwtDenylist,
-       omniauth_providers: [:google_oauth2, :facebook]
+         :recoverable, :rememberable, :validatable,
+         :confirmable,
+         :jwt_authenticatable,
+         :omniauthable,
+         jwt_revocation_strategy: JwtDenylist,
+         omniauth_providers: [:google_oauth2, :facebook]
 
+  # 🔥 ADD THIS METHOD HERE
   def self.from_omniauth(auth)
-    user = User.find_by(email: auth.info.email)
+    email = auth.info.email
 
-    if user
-      # 🔥 Account merge
-      user.update(
-        provider: auth.provider,
-        uid: auth.uid
-      )
-    else
-      user = User.create!(
-        email: auth.info.email,
-        password: Devise.friendly_token[0, 20],
-        provider: auth.provider,
-        uid: auth.uid,
-        name: auth.info.name,
-        confirmed_at: Time.current # 🔥 skip email confirmation
-      )
-    end
+    return nil unless email.present?   # prevent crash
+
+    user = User.find_or_initialize_by(email: email)
+
+    user.update!(
+      provider: auth.provider,
+      uid: auth.uid,
+      name: auth.info.name,
+      password: Devise.friendly_token[0, 20],
+      confirmed_at: Time.current
+    )
 
     user
   end
