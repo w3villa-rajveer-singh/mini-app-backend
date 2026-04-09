6f325faed1c9aa2b12e1d97b7b3bbc278da12029
Rajveer Singh <rajveer.singh@w3villa.com>
2026-04-09 18:16:09 +0530

    changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,5 @@ User.find_or_create_by!(email: "admin@gmail.com") do |u|
   u.password = "admin123"
   u.password_confirmation = "admin123"
   u.admin = true
+  u.confirmed_at = Time.current   # 🔥 IMPORTANT
 end
